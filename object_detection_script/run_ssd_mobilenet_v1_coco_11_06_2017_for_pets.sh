3856208222f66344231f6d3b74c8ca1a34af4b2d
robin <zhangbinatp@gmail.com>
2018-07-13 11:04:49 +0800

    add_some_script
    Signed-off-by: robin <zhangbinatp@gmail.com>

---
+++ object_detection_script/run_ssd_mobilenet_v1_coco_11_06_2017_for_pets.sh
@@ -0,0 +1,76 @@
+/home/zhangbin/SoftWare/protoc_3.3/bin/protoc object_detection/protos/*.proto --python_out=.
+export PYTHONPATH=
+export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+echo $PYTHONPATH
+
+PATH_TO_YOUR_PIPELINE_CONFIG=`pwd`/object_detection/01_pets_tf_tutorials/models/ssd_mobilenet_v1_coco_2017_11_17/ssd_mobilenet_v1_pets.config
+PATH_TO_TRAIN_DIR=`pwd`/object_detection/01_pets_tf_tutorials/models/ssd_mobilenet_v1_coco_2017_11_17/train
+PATH_TO_EVAL_DIR=`pwd`/object_detection/01_pets_tf_tutorials/models/ssd_mobilenet_v1_coco_2017_11_17/eval
+
+#PATH_TO_YOUR_PIPELINE_CONFIG=`pwd`/object_detection/01_pets_tf_tutorials/models/ssd_inception_v2_coco_2017_11_17/ssd_inception_v2_pets.config
+#PATH_TO_TRAIN_DIR=`pwd`/object_detection/01_pets_tf_tutorials/models/ssd_inception_v2_coco_2017_11_17/train
+echo $PATH_TO_YOUR_PIPELINE_CONFIG
+##test the env
+python object_detection/builders/model_builder_test.py
+
+
+echo "input command:train or eval or export:"
+read a
+echo "input is $a"
+
+
+##create the tfrecord files
+#python object_detection/create_pet_tf_record.py \
+#    --label_map_path=`pwd`/object_detection/pets_tf_tutorials/data/pet_label_map.pbtxt \
+#    --data_dir=`pwd`/object_detection/pets_tf_tutorials/data/dataset/ \
+#    --output_dir=`pwd`/object_detection/pets_tf_tutorials/data/
+
+if [ $a = train ] ; then
+
+## From the tensorflow/models/research/ directory
+#python object_detection/train.py \
+#    --logtostderr \
+#    --pipeline_config_path=${PATH_TO_YOUR_PIPELINE_CONFIG} \
+#    --train_dir=${PATH_TO_TRAIN_DIR}
+###################################################################
+
+python object_detection/train.py \
+    --logtostderr \
+   --pipeline_config_path=${PATH_TO_YOUR_PIPELINE_CONFIG} \
+    --train_dir=${PATH_TO_TRAIN_DIR}
+fi
+
+
+if [ $a = eval ] ; then
+## From the tensorflow/models/research/ directory
+#python object_detection/eval.py \
+#    --logtostderr \
+#    --pipeline_config_path=${PATH_TO_YOUR_PIPELINE_CONFIG} \
+#    --checkpoint_dir=${PATH_TO_TRAIN_DIR} \
+#    --eval_dir=${PATH_TO_EVAL_DIR}
+###################################################################
+
+python object_detection/eval.py \
+    --logtostderr \
+    --pipeline_config_path=${PATH_TO_YOUR_PIPELINE_CONFIG} \
+    --checkpoint_dir=${PATH_TO_TRAIN_DIR} \
+    --eval_dir=${PATH_TO_EVAL_DIR}
+fi
+
+
+if [ $a = export ] ; then
+## From tensorflow/models/research/
+#python object_detection/export_inference_graph.py \
+#    --input_type image_tensor \
+#    --pipeline_config_path ${PIPELINE_CONFIG_PATH} \
+#    --trained_checkpoint_prefix ${TRAIN_PATH} \
+#    --output_directory output_inference_graph.pb
+###################################################################
+
+python object_detection/export_inference_graph.py \
+    --input_type image_tensor \
+    --pipeline_config_path=${PATH_TO_YOUR_PIPELINE_CONFIG} \
+    --trained_checkpoint_prefix=`pwd`/object_detection/01_pets_tf_tutorials/models/ssd_mobilenet_v1_coco_2017_11_17/train/model.ckpt-346 \
+    --output_directory=`pwd`/object_detection/01_pets_tf_tutorials/models/ssd_mobilenet_v1_coco_2017_11_17/fine_tuned_model/model-346
+fi
+
